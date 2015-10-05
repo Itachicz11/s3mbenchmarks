da3e0df07b4092983553496815fb01927e6373c2
Jakub Kohout <jakub.kohout11@gmail.com>
2015-10-05 12:51:09 -0400

    reset password mail

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'jakub.kohout11@gmail.com', 'name' => 'Jakub Kohout'],
 
     /*
     |--------------------------------------------------------------------------
