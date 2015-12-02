c202c2db230793c51ddf2caf2382a7783b7f2411
Jakub Kohout <jakub.kohout11@gmail.com>
2015-12-02 17:49:16 -0500

    preventing calling chart on wrong place

--- public/js/app.js
+++ public/js/app.js
@@ -6,32 +6,32 @@
 (function() {
   var drawChart;
 
-  $.ajax({
-    url: '/callrail/users_companies_data',
-    type: "GET",
-    dataType: "JSON",
-    success: function(data) {
-      return google.setOnLoadCallback(drawChart(data));
-    }
-  });
-
-  drawChart = function(chart_data) {
-    var chart, data, options;
-    data = new google.visualization.DataTable();
-    data.addColumn("string", "User");
-    data.addColumn("number", "Companies");
-    data.addRows(chart_data);
-    options = {
-      width: "100%",
-      height: 300
+  if ($('#chart_div').length) {
+    $.ajax({
+      url: '/callrail/users_companies_data',
+      type: "GET",
+      dataType: "JSON",
+      success: function(data) {
+        return google.setOnLoadCallback(drawChart(data));
+      }
+    });
+    drawChart = function(chart_data) {
+      var chart, data, options;
+      data = new google.visualization.DataTable();
+      data.addColumn("string", "User");
+      data.addColumn("number", "Companies");
+      data.addRows(chart_data);
+      options = {
+        width: "100%",
+        height: 300
+      };
+      chart = new google.visualization.PieChart(document.getElementById("chart_div"));
+      return chart.draw(data, options);
     };
-    chart = new google.visualization.PieChart(document.getElementById("chart_div"));
-    return chart.draw(data, options);
-  };
-
-  google.load("visualization", "1.0", {
-    packages: ["corechart"]
-  });
+    google.load("visualization", "1.0", {
+      packages: ["corechart"]
+    });
+  }
 
 }).call(this);
 
